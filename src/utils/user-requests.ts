899cf598de9f13c84f678fcda6a7c7134aac40bc
AngryKat <gatto_candido@ukr.net>
2022-12-11 12:42:24 +0200

    Increase limit for items per page

--- src/utils/user-requests.ts
+++ src/utils/user-requests.ts
@@ -7,22 +7,21 @@ export const getUsers = async (page?: number) => {
     try {
         let requestPath = '/users';
         if (page) {
-            requestPath += `?_page=${page}`;
+            requestPath += `?_page=${page}&_limit=20`;
         }
-        const { data } = await sendRequest(requestPath);
-        return data;
-    } catch ({ message }) {
-        toast.error(`Error occurred while fetching users: ${message} `)
-
-    }
+        const response = await sendRequest(requestPath);
+        return response;
+    } catch (error) {
+        toast.error('Error occurred while fetching users')
+    };
 };
 
 export const addUser = async (newUserData: UserFormValues) => {
     try {
-        const { data } = await sendRequest('/users', 'POST', { id: uuidv4(), ...newUserData });
+        const response = await sendRequest('/users', 'POST', { id: uuidv4(), ...newUserData });
         toast.success('Added new user successfully!')
-        return data;
-    } catch ({ message }) {
-        toast.error(`Error occurred while adding user: ${message} `)
+        return response;
+    } catch (error) {
+        toast.error('Error occurred while adding user:');
     }
 };
